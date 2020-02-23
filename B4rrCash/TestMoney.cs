56d917469c46d677f992cc8f34e872348a873066
Stefan Lochau <lochaus@hotmail.com>
2020-02-23 14:51:39 +0100

    Added first test an constructor for Dollar class.

---
+++ B4rrCash/TestMoney.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B4rrCash
+{
+    [TestFixture]
+    public class TestMoney
+    {
+        [Test]
+        public void TestMultiplication()
+        {
+            Dollar fiveDollars = new Dollar(5);
+            Dollar tenDollars = new Dollar(10);
+            Assert.AreEqual(tenDollars, 2 * fiveDollars);
+        }
+    }
+}
