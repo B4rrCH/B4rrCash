18427068d3f77c2d29fa999740ec42279d267e77
Stefan Lochau <lochaus@hotmail.com>
2020-02-23 16:24:27 +0100

    Got rid of Money sub-classes.

--- B4rrCash/Money.cs
+++ B4rrCash/Money.cs
@@ -6,27 +6,36 @@ using System.Threading.Tasks;
 
 namespace B4rrCash
 {
-    abstract class Money
+    class Money
     {
         public int Amount { get; protected set; }
         public string Currency { get; protected set; }
+
+        public Money(int amount = 1, string currency = "USD")
+        {
+            Amount = amount;
+            Currency = currency;
+        }
 
         public override bool Equals(object obj)
         {
             return obj is Money money &&
                    Amount == money.Amount &&
-                   GetCurrency() == money.GetCurrency();
+                   Currency == money.Currency;
         }
 
         public override int GetHashCode()
         {
             var hashCode = -259941593;
             hashCode = hashCode * -1521134295 + Amount.GetHashCode();
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(GetCurrency());
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Currency);
             return hashCode;
         }
 
-        public abstract Money times(int factor);
+        public Money times(int factor)
+        {
+            return new Money(factor * Amount, Currency);
+        }
 
         public static Money operator *(int factor, Money money)
         {
@@ -37,7 +46,7 @@ namespace B4rrCash
             return money.times(factor);
         }
 
-        public static Money Dollar(int amount) => amount * new Dollar();
-        public static Money Franc(int amount) => amount * new Franc();
+        public static Money Dollar(int amount) => new Money(amount, "USD");
+        public static Money Franc(int amount) => new Money(amount, "CHF");
     }
 }
